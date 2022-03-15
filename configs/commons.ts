d9d74c279f4b0ba344038ea3098d0521424c3246
Thor Seldon <thorseldon@protonmail.com>
2022-03-15 15:39:38 +0800

    add kovan testnet

--- configs/commons.ts
+++ configs/commons.ts
@@ -23,6 +23,7 @@ export const CommonsConfig: ICommonConfiguration = {
     [eEthereumNetwork.localhost]: undefined,
     [eEthereumNetwork.develop]: '0xFA2975862160e2E3A522f40c59174a9Fd0b5D67B',
     [eEthereumNetwork.rinkeby]: '0x8601bCbDC41a6Fe6422764EF1Dc8ccb7fa8B891B',
+    [eEthereumNetwork.kovan]: '0x6606FF6624139a1599522Cf7B401b042Da43fA4f',
     [eEthereumNetwork.main]: undefined,
   },
 
@@ -32,14 +33,16 @@ export const CommonsConfig: ICommonConfiguration = {
     [eEthereumNetwork.localhost]: '',
     [eEthereumNetwork.develop]: '',
     [eEthereumNetwork.rinkeby]: '0xB873F088EB721261bc88BbC739B5C794e02e414b',
+    [eEthereumNetwork.kovan]: '0xC5d1624B46db4F3F628400C0F41c49220c210c3F',
     [eEthereumNetwork.main]: '',
   },
   BNFTRegistryOwner: {
     [eEthereumNetwork.coverage]: '',
     [eEthereumNetwork.hardhat]: '',
     [eEthereumNetwork.localhost]: '0xafF5C36642385b6c7Aaf7585eC785aB2316b5db6',
-    [eEthereumNetwork.develop]: '',
+    [eEthereumNetwork.develop]: '0xad93fB0e59eC703422dD38dCb7AcB8e323C8cc5B',
     [eEthereumNetwork.rinkeby]: '0xafF5C36642385b6c7Aaf7585eC785aB2316b5db6',
+    [eEthereumNetwork.kovan]: '0x249D0dF00d8ca96952A9fc29ddD3199bD035A05B',
     [eEthereumNetwork.main]: '',
   },
 
@@ -49,6 +52,7 @@ export const CommonsConfig: ICommonConfiguration = {
     [eEthereumNetwork.localhost]: {},
     [eEthereumNetwork.main]: {},
     [eEthereumNetwork.rinkeby]: {},
+    [eEthereumNetwork.kovan]: {},
     [eEthereumNetwork.develop]: {},
   },
 };
